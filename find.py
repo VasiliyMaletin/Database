aa20c468408920ec31f85ee88173f1418bd8fc04
Sokem <sokems@mail.ru>
2022-11-20 22:35:06 +0500

    edit find.py

--- find.py
+++ find.py
@@ -1,11 +1,74 @@
 import json
 
-# def request_filter_data_to_list(find_pupil): # Поиск ученика по заданному словарю.
-#     with open('pupils.json', 'r', encoding='utf-8') as file:
-#         list_pupils = json.load(file)
-#
-#     pupil_list = []
-#     return pupil_list
+def request_filter_data_to_list(find_pupil): # Поиск ученика по заданному словарю.
+    with open('pupils.json', 'r', encoding='utf-8') as file:
+        list_pupils = json.load(file)
+    pupil_list = []
+    for i in list_pupils:
+        check = 0
+        if i["ФИО"] != " ":
+            print(i["ФИО"])
+            a = find_pupil["ФИО"]
+            b = i["ФИО"]
+            if a in b:
+                check = 1
+            else:
+                check = 0
+                continue
+
+        if i["Класс"] != " ":
+            print(i["Класс"])
+            a = find_pupil["Класс"]
+            b = i["Класс"]
+            if a in b:
+                check = 1
+            else:
+                check = 0
+                continue
+
+        if i["Руководитель"] != " ":
+            print(i["Руководитель"])
+            a = find_pupil["Руководитель"]
+            b = i["Руководитель"]
+            if a in b:
+                check = 1
+            else:
+                check = 0
+                continue
+
+        if i["Успеваемость"] != " ":
+            print(i["Успеваемость"])
+            a = find_pupil["Успеваемость"]
+            b = i["Успеваемость"]
+            if a in b:
+                check = 1
+            else:
+                check = 0
+                continue
+
+        if i["Год рождения"] != " ":
+            print(i["Год рождения"])
+            a = find_pupil["Год рождения"]
+            b = i["Год рождения"]
+            if a == b:
+                check = 1
+            else:
+                check = 0
+                continue
+
+        if i["Телефон"] != " ":
+            print(i["Телефон"])
+            a = find_pupil["Телефон"]
+            b = i["Телефон"]
+            if a == b:
+                check = 1
+            else:
+                check = 0
+                continue
+
+        if check == 1:
+            pupil_list.append(i)
+    return pupil_list
 
 def request_all_data_to_list(): # Возвращает полный список учеников
     with open('pupils.json', 'r', encoding='utf-8') as file:
@@ -20,4 +83,5 @@ def request_pupil_data(id): # Возвращает словарь данных �
             pupil_dict = i
     return pupil_dict
 
-# print(request_pupil_data(1))
+find_pupil = {"id": " ", "ФИО": "Иванов", "Класс": " ", "Руководитель": " ", "Успеваемость": " ", "Год рождения": 2012, "Телефон": " "}
+print(request_filter_data_to_list(find_pupil))
