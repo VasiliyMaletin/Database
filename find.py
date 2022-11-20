02941e3b368c098b178a7f959758d597de53429f
Sokem <sokems@mail.ru>
2022-11-20 18:51:58 +0500

    edit json

--- find.py
+++ find.py
@@ -1,16 +1,20 @@
-def request_filter_data_to_list(filter_field, filter_string):
-    # Пробегается по всему списку учеников и возвращает список учеников, удовлетворяющих фильтру
-    # filter_field, filter_string - списки имен полей, по которым нужно отфильтровать 
-    # и значения этих полей для фильтрации
-    pupil_list = []
-    return pupil_list
+# def request_filter_data_to_list(filter_field, filter_string):
+#     # Пробегается по всему списку учеников и возвращает список учеников, удовлетворяющих фильтру
+#     # filter_field, filter_string - списки имен полей, по которым нужно отфильтровать
+#     # и значения этих полей для фильтрации
+#     pupil_list = []
+#     return pupil_list
+#
+# def request_all_data_to_list():
+#     # Возвращает полный список учеников
+#     pupil_list = []
+#     return pupil_list
+#
+# def request_pupil_data(id):
+#     #Возвращает словарь данных по конкретному ученику по его id
+#     pupil_dict = {}
+#     return pupil_dict
 
-def request_all_data_to_list():
-    # Возвращает полный список учеников
-    pupil_list = []
-    return pupil_list
-
-def request_pupil_data(id):
-    #Возвращает словарь данных по конкретному ученику по его id
-    pupil_dict = {}
-    return pupil_dict
+with open('pupils.json', 'r') as file:
+    list_pupils = file
+    print(list_pupils)
