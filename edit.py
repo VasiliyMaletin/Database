03771d9bbb24aee94f51cae6e23531f675b9f176
Vasiliy Maletin <maletinvasilii@mail.ru>
2022-11-24 21:41:18 +0800

    push my files

--- edit.py
+++ edit.py
@@ -4,12 +4,25 @@ from logs import add_log
 from find import request_all_data_to_list, request_pupil_data
 id = 2
 temp_pupil_dict = {"id": 2, "ФИО": "Петров Иван Иванович", "Класс": "10В", "Руководитель": "Сидоренко Арина Николаевна", "Успеваемость": "Хорошист", "Год рождения": "2011", "Телефон": "89174455584"}
-def save_data(id):
+def save_data(save_pupil_dict):
     pupil_dict = request_all_data_to_list()
-    temp_pupil_dict = request_pupil_data(id)
-    for index, key in enumerate(pupil_dict):
-        if key["id"] == temp_pupil_dict["id"]:
-            print(pupil_dict)
+    id_found = False
+    for i in range(len(pupil_dict)):
+        if save_pupil_dict["id"] == pupil_dict[i]["id"]:
+            pupil_dict[i] = save_pupil_dict
+            id_found = True
+    if id_found == False:
+        pupil_dict.append(save_pupil_dict)
+    with open('pupils.json', 'w', encoding='utf-8') as file:
+                json.dump(pupil_dict, file, ensure_ascii=False)
+    if id_found == False:
+         add_log(f'Новый ученик с id {save_pupil_dict["id"]} успешно добавлен')
+    else:
+        add_log(f'Изменения в id {save_pupil_dict["id"]} успешно сохранены')
+
+    # new_pupil_dict = request_pupil_data(id)
+    # for index, key in enumerate(pupil_dict):
+    #     if key["id"] == temp_pupil_dict["id"]:
             # temp = temp_pupil_dict
             # temp.append(data_new)
             # key['data'] = temp
@@ -19,13 +32,11 @@ def save_data(id):
             # обновить данные в json файл по id данными из pupil_dict
             # pupil_list = request_all_data_to_list()
             # print(pupil_list)
-            # with open('pupils.json', 'w', encoding='utf-8') as file:
-            #     pupil_list = json.(file)
+            
         # else:
         #     pupil_dict = temp_pupil_dict
             # добавить новую строку в json файл из pupil_dict
-        
-print(save_data(id))
+    
 
 
     # Переписать словарь ученика с указанным id (Есть в pupil_dict) новыми данными из pupil_dict в json файле
@@ -33,4 +44,4 @@ print(save_data(id))
 
     # listbox_fill(pupil_dict)
     # print(f'Изменения сохранены по id {pupil_dict["id"]}')
-    # add_log()
+   
