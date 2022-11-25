d03b9d8914f0edbc4f3f9d9cddc4a9fa3f09eaf8
Vasiliy Maletin <108918665+VasiliyMaletin@users.noreply.github.com>
2022-11-25 09:43:46 +0800

    Update logs.py
    edited log

--- logs.py
+++ logs.py
@@ -5,6 +5,6 @@ from datetime import datetime
 def add_log(log_record):
     time = datetime.now().strftime('%H:%M:%S')
     with open('log.csv', 'a', encoding='utf-8') as log:
-        log.write('{} Действие: {}\n'
+        log.write('{} Action: {}\n'
                     .format(time, log_record))
 
