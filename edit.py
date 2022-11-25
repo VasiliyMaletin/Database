9068be8205b6f903439570728824728349190c25
Vasiliy Maletin <108918665+VasiliyMaletin@users.noreply.github.com>
2022-11-25 09:46:21 +0800

    Update edit.py

--- edit.py
+++ edit.py
@@ -1,9 +1,9 @@
+# Переписать словарь ученика с указанным id (Есть в pupil_dict) новыми данными из pupil_dict в json файле
+# Если записи с указанным id не существует, создать эту новую запись с переданными данными
+
 import json
 from logs import add_log
-# from interface import get_pupil_dict
-from find import request_all_data_to_list, request_pupil_data
-# id = 2
-# temp_pupil_dict = {"id": 2, "ФИО": "Петров Иван Иванович", "Класс": "10В", "Руководитель": "Сидоренко Арина Николаевна", "Успеваемость": "Хорошист", "Год рождения": "2011", "Телефон": "89174455584"}
+from find import request_all_data_to_list
 
 def save_data(save_pupil_dict):
     pupil_dict = request_all_data_to_list()
@@ -17,32 +17,6 @@ def save_data(save_pupil_dict):
     with open('pupils.json', 'w', encoding='utf-8') as file:
                 json.dump(pupil_dict, file, ensure_ascii=False)
     if id_found == False:
-         add_log(f'Новый ученик с id {save_pupil_dict["id"]} успешно добавлен')
+         add_log(f'New pupil with id {save_pupil_dict["id"]} added successfully')
     else:
-        add_log(f'Изменения в id {save_pupil_dict["id"]} успешно сохранены')
-
-    # new_pupil_dict = request_pupil_data(id)
-    # for index, key in enumerate(pupil_dict):
-    #     if key["id"] == temp_pupil_dict["id"]:
-            # temp = temp_pupil_dict
-            # temp.append(data_new)
-            # key['data'] = temp
-            # data[index] = key
-            # break
-
-            # обновить данные в json файл по id данными из pupil_dict
-            # pupil_list = request_all_data_to_list()
-            # print(pupil_list)
-            
-        # else:
-        #     pupil_dict = temp_pupil_dict
-            # добавить новую строку в json файл из pupil_dict
-    
-
-
-    # Переписать словарь ученика с указанным id (Есть в pupil_dict) новыми данными из pupil_dict в json файле
-    # Если записи с указанным id не существует, создать эту новую запись с переданными данными
-
-    # listbox_fill(pupil_dict)
-    # print(f'Изменения сохранены по id {pupil_dict["id"]}')
-   
+        add_log(f'Changes to id {save_pupil_dict["id"]} saved successfully')
